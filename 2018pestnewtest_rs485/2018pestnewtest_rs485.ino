db92a06db54db9d80dc173d29b2d6e8372f91b94
ChiShengChen <m50816m50816@gmail.com>
2018-04-05 00:07:37 +0800

    rs485

---
+++ 2018pestnewtest_rs485/2018pestnewtest_rs485.ino
@@ -0,0 +1,110 @@
+//ir沒遮斷 == HIGH
+//for arduino nano
+#include <SoftwareSerial.h> 
+#define SSerialRX 0
+#define SSerialTX 1
+#define SSerialTxControl 3   //RS485 Direction control 
+#define RS485Transmit HIGH 
+#define RS485Receive LOW 
+#define Pin12LED 12 
+SoftwareSerial RS485Serial(SSerialRX, SSerialTX); // RX, TX 
+int byteReceived;
+int byteSend; 
+
+int bcc1 = 4;
+int bcc2 = 5;
+int bcc3 = 6;
+int LED1 = 8;
+int LED2 = 9;
+int LED3 = 10;
+int ch1, ch2, ch3 = 0;
+unsigned int t = 500;
+unsigned int pestsum = 0;
+
+void setup() {
+Serial.begin(9600);   
+Serial.println("SerialRemote");  // Can be ignored     
+pinMode(Pin12LED, OUTPUT);    
+pinMode(SSerialTxControl, OUTPUT);      
+digitalWrite(SSerialTxControl, RS485Receive);  // Init Transceiver     
+// Start the software serial port, to another device   
+RS485Serial.begin(4800);   // set the data rate 
+  
+pinMode(bcc1, INPUT_PULLUP);
+pinMode(bcc2, INPUT_PULLUP);
+pinMode(bcc3, INPUT_PULLUP);
+pinMode(LED1, OUTPUT);
+pinMode(LED2, OUTPUT);
+pinMode(LED3, OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int mode1, mode2, mode3  ;
+
+if (digitalRead(bcc1) == HIGH)
+{
+  mode1 = 1;
+  digitalWrite(LED1, HIGH);
+}
+else
+{
+  mode1 = 0;
+  digitalWrite(LED1, LOW);
+  ch1++;
+}
+
+if (digitalRead(bcc2) == HIGH)
+{
+  mode2 = 1;
+  digitalWrite(LED2, HIGH);
+}
+else
+{
+  mode2 = 0;
+  digitalWrite(LED2, LOW);
+  ch2++;
+}
+
+if (digitalRead(bcc3) == HIGH)
+{
+  mode3 = 1;
+  digitalWrite(LED3, HIGH);
+}
+else
+{
+  mode3 = 0;
+  digitalWrite(LED3, LOW);
+  ch3++;
+}
+
+pestsum = ch1+ch2+ch3;
+
+//Copy input data to output    
+if (RS485Serial.available())    
+{     
+  pestsum = byteSend;
+  byteSend = RS485Serial.read();   // Read the byte        
+  digitalWrite(Pin12LED, HIGH);  // Show activity     
+  delay(10);      
+  digitalWrite(Pin12LED, LOW);        
+  
+  digitalWrite(SSerialTxControl, RS485Transmit);  // Enable RS485 Transmit      
+  RS485Serial.write(byteSend); // Send the byte back     
+  delay(10);      
+  //digitalWrite(SSerialTxControl, RS485Receive);  // Disable RS485 Transmit  
+  //delay(100); // End If RS485SerialAvailable   
+}
+
+/*Serial.print("ch1:");
+Serial.println(ch1);
+Serial.print("ch2:");
+Serial.println(ch2);
+Serial.print("ch3:");
+Serial.println(ch3);
+Serial.print("pestsum:");
+Serial.println(pestsum);
+Serial.println("-------------------------");*/
+delay(t);
+}
